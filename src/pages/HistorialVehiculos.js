338742c005d9492d00e4be2ef55f0d8ceb1afbe9
Vivibg <155114409+Vivibg@users.noreply.github.com>
2025-07-27 22:55:37 -0400

    Update HistorialVehiculos.js

--- src/pages/HistorialVehiculos.js
+++ src/pages/HistorialVehiculos.js
@@ -9,7 +9,7 @@ function HistorialVehiculos() {
   const [busqueda, setBusqueda] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:4000/api/historial')
+    fetch(`${process.env.REACT_APP_API_URL}/historial`)
       .then(res => res.json())
       .then(data => Array.isArray(data) ? setHistoriales(data) : setHistoriales([]))
       .catch(() => setHistoriales([]));
